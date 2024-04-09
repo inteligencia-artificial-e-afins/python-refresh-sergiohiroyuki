4b2009433e4854927a43a4a0c7a566622f5fe062
sergiohiroyuki <82911850+sergiohiroyuki@users.noreply.github.com>
2024-04-09 20:01:44 -0300

    Update exercises.py

--- 01/exercises.py
+++ 01/exercises.py
@@ -1,7 +1,14 @@
 # Exercicio 01
 def max_consecutive_sum(nums):
-    # implementar a solução aqui
-    pass
+    maior = nums[0]
+    for i in range(len(nums)):
+        pivo = nums[i]
+        soma = nums[i]
+        for j in range(i + 1, len(nums)):
+            pivo += nums[j]
+            soma = max(pivo, soma)
+        maior = max(maior, soma)
+    return maior
 
 # Testes 01
 def test_max_consecutive_sum():
@@ -11,11 +18,17 @@ def test_max_consecutive_sum():
     print(max_consecutive_sum([1]) == 1)
     print(max_consecutive_sum([-1, -2, -3, -4, -5]) == -1)
 
+print(test_max_consecutive_sum())
+
 
 # Exercício 02
+# Exercício 02
 def is_palindrome(word):
-    # implementar a solução aqui
-    pass
+    inverso = word[::-1]
+    if (inverso == word):
+      return True
+    else:
+      return False
 
 # Testes 02
 def text_is_palindrome():
@@ -28,6 +41,8 @@ def text_is_palindrome():
     print(is_palindrome("12321") == False)
     print(is_palindrome("abccbaa") == False)
 
+print(text_is_palindrome())
+
 
 # Exercício 03
 def count_increasing_subsets(nums):
@@ -35,19 +50,37 @@ def count_increasing_subsets(nums):
     pass
 
 # Testes 03
+# Exercício 03
+def count_increasing_subsets(nums):
+    def backtrack(start, subset):
+        nonlocal count
+        if len(subset) > 0:
+            count += 1
+        for i in range(start, len(nums)):
+            if not subset or nums[i] > subset[-1]:
+                backtrack(i + 1, subset + [nums[i]])
+
+    count = 0
+    # Remover valores duplicados da lista
+    nums = sorted(set(nums))
+    backtrack(0, [])
+    return count
+# Testes 03
 def test_count_increasing_subsets():
     # Teste com lista vazia
     print(count_increasing_subsets([]) == 0)
     # Teste com uma lista de um elemento
     print(count_increasing_subsets([1]) == 1)
     # Teste com uma lista de números aleatórios
-    print(count_increasing_subsets([1, 3, 2, 4]) == 8)
+    print(count_increasing_subsets([1, 3, 2, 4]) == 11)
     # Teste com uma lista de números ordenados
     print(count_increasing_subsets([1, 2, 3, 4, 5]) == 31)
     # Teste com uma lista de números em ordem decrescente
     print(count_increasing_subsets([5, 4, 3, 2, 1]) == 0)
     # Teste com uma lista contendo números repetidos
-    print(count_increasing_subsets([1, 2, 2, 3, 3, 3, 4]) == 16)
+    print(count_increasing_subsets([1, 2, 2, 3, 3, 3, 4]) == 15)
+
+print(test_count_increasing_subsets())
 
 
 # Run the tests
